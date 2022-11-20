b1fe5f9056268102ad1f74a825ec056f1a85f448
Mateusz <matt.borowczyk@gmail.com>
2022-11-20 02:07:57 +0100

    init navigation for other screens

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useState } from 'react'
 import { transactions } from './src/constants/data'
 import HomeScreen from './src/screens/HomeScreen'
+import StatisticsScreen from './src/screens/StatisticsScreen'
+import TransactionScreen from './src/screens/TransactionScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -17,6 +19,46 @@ export default function App() {
         <Stack.Screen name='Home' options={{ headerShown: false }}>
           {(props) => (
             <HomeScreen {...props} user={user} transactions={allTransactions} />
+          )}
+        </Stack.Screen>
+        <Stack.Screen
+          name='Statistics'
+          options={{
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title='Info'
+                color='#fff'
+              />
+            ),
+          }}
+        >
+          {(props) => (
+            <StatisticsScreen
+              {...props}
+              user={user}
+              transactions={allTransactions}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen
+          name='Transactions'
+          options={{
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title='Info'
+                color='#fff'
+              />
+            ),
+          }}
+        >
+          {(props) => (
+            <TransactionScreen
+              {...props}
+              user={user}
+              transactions={allTransactions}
+            />
           )}
         </Stack.Screen>
       </Stack.Navigator>
